09a0c13fb16707bb58df0b776dcf5638f80ee1d7
Edward Potocko <epotocko@gmail.com>
2011-04-12 11:12:30 -0400

    Fixing issues with the :not() selector and IE

--- src/rails.js
+++ src/rails.js
@@ -109,7 +109,7 @@ YUI.add('rails-ujs', function(Y) {
 }, '0.0.1', { requires: ['node-base', 'event-synthetic'] });
 
 
-YUI().use('node-base', 'node-event-delegate', 'io-form', 'rails-ujs', 'rails-event-bubble', function(Y) {
+YUI().use('node-base', 'node-event-delegate', 'selector-css3', 'io-form', 'rails-ujs', 'rails-event-bubble', function(Y) {
 
 	var doc = Y.one(document);
 
